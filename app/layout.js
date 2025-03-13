876fe98d68fe2232062c5a2f02514ca7e3258501
tushar20082002 <tusharkantibhattacharya31@gmail.com>
2025-03-13 22:44:00 +0530

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+
 
 const inter=Inter({subsets:["latin"]});
 
@@ -11,6 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${inter.className}`}>
@@ -29,5 +32,6 @@ export default function RootLayout({ children }) {
           </footer>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
